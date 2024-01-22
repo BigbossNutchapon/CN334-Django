8a0aa0a7d625f1f5cfbbb4347e2b1cae0af24409
BigbossNutchapon <nutchapon2003@gmail.com>
2024-01-22 16:14:26 +0700

    add api student info

---
+++ ecommerce/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+
+def ecommerce_index_view(request):
+    return HttpResponse('Welcome to 6410742156 Nutchapon Kitkram views!')
+
+def item_view(request, item_id): 
+    context_data = {
+        "item_id": item_id
+        }
+    
+    return render(request, 'index.html',context= context_data)
