a69c11e51a82e50170164521d7a57248cb69a3ee
BigbossNutchapon <nutchapon2003@gmail.com>
2024-01-28 15:20:30 +0700

    add page

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -10,3 +10,18 @@ def item_view(request, item_id):
         }
     
     return render(request, 'index.html',context= context_data)
+
+def ecommerce_home_view(request):
+    return render(request, 'home.html')
+
+def ecommerce_category_view(request):
+    return render(request, 'category.html')
+
+def ecommerce_contact_view(request):
+    return render(request, 'contact.html')
+
+def ecommerce_product_view(request):
+    return render(request, 'product.html')
+
+def ecommerce_checkout_view(request):
+    return render(request, 'checkout.html')
